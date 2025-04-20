8fa8b1ba7bfd1b2a62802e0405a944d2c30933d3
sjnhnp <18591921+sjnhnp@users.noreply.github.com>
2025-04-20 21:30:10 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Output dir: dist (default, Cloudflare Pages compatible)
+  // If you later want to use an 'assets' base, add: base: './'
+});
