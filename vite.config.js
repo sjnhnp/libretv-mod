0bb595fcb6c590def67f22fa54978fb76ed88df4
sjnhnp <18591921+sjnhnp@users.noreply.github.com>
2025-04-21 10:01:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
-
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: 'index.html',
-        player: 'public/player.html' // 或 'player.html' 视你的public配置
+        player: 'player.html'
       }
     }
   }
