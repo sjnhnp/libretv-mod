dd6b27d64d6d3780d087db898f6b87174290671e
sjnhnp <18591921+sjnhnp@users.noreply.github.com>
2025-04-21 09:53:32 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Output dir: dist (default, Cloudflare Pages compatible)
-  // If you later want to use an 'assets' base, add: base: './'
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        player: 'public/player.html' // 或 'player.html' 视你的public配置
+      }
+    }
+  }
 });
